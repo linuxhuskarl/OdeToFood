01dc6d1e7a137857a62c949038cc028618b6af76
linuxhuskarl <linuxhuskarl@gmail.com>
2020-02-12 11:44:21 +0100

    Added Core class library and Data class library

--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -4,13 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
+        private readonly IConfiguration configuration;
+        private readonly IRestaurantData restaurantData;
+        public string Message { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+        public ListModel(IConfiguration configuration, IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+            this.configuration = configuration;
+        }
         public void OnGet()
         {
+            Message = "Hello, World!";
+            Restaurants = restaurantData.GetAll();
         }
     }
 }
